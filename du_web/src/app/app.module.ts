e5b3d1531fa7ef28e1ddb0460619ee65b8d96990
sritheja312 <sritheja.pothooru@antra.com>
2024-07-16 20:08:41 +0530

    flooring component completed design
    flooring component completed design

--- du_web/src/app/app.module.ts
+++ du_web/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FlooringComponent } from './flooring/flooring.component';
+import { PainterComponent } from './painter/painter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    FlooringComponent
+    FlooringComponent,
+    PainterComponent
   ],
   imports: [
     BrowserModule,
